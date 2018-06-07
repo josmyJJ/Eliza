fab9c3b96cc7d046daacd8023e17a40e9bb47372
Josemy Joseph <josemyjoseph@gmail.com>
2018-06-06 20:37:27 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,75 +5,75 @@ import java.io.BufferedReader;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-         Boolean userQuit = true;
-        String userRespo;
-
+    public static void main(String[] args) throws IOException {
+		
+		Random rand = new Random();
+		BufferedReader keyboardInput = null;
+		
+		// Variable
+		String userInput;
+		String[] splitStr; // splitStr array declared
+		
+		 // array list to store hedges and qualifiers
         String[] hedges = {
                 "Please tell me more", "Many of my patients tell me the same thing",
                 "It is getting late, maybe we had better quit"
         };
-        String[] qualifiers = {
+        String qualifiers[] = {
                 "Why do you say that", "You seem to think that",
                 "So, you are concerned that"
         };
-
-        BufferedReader keyboardInput = null;
+        
+     // BufferedReader reads the user input
         keyboardInput = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Good day. What is your problem? ");
-        System.out.println("Enter your response here or Q to quit:");
-
-        do{
-            userRespo = keyboardInput.readLine();
-
-            String outputStr = "";
-            String[] splitStr = userRespo.split(" ");
-
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put("I", "you");
-            map.put("me", "you");
-            map.put("my", "your");
-            map.put("am", "are");
-
-            for(int i=0; i<splitStr.length; i++){
-                if(splitStr[i].equalsIgnoreCase("my")){
-                    splitStr[i] = map.get("my");
-                }
-                if(splitStr[i].equalsIgnoreCase("me")){
-                    splitStr[i] = map.get("me");
-                }
-                if(splitStr[i].equalsIgnoreCase("i")){
-                    splitStr[i] = map.get("i");
-                }
-                if(splitStr[i].equalsIgnoreCase("am")){
-                    splitStr[i] = map.get("am");
-                }
-            }
-
-
-            for(int i=0; i<splitStr.length; i++){
-                outputStr += splitStr[i] + " ";
-            }
-
-            Random rand = new Random();
-            int randQuali = rand.nextInt(qualifiers.length);
-            int randHed = rand.nextInt(hedges.length);
-
-            int response =rand.nextInt(2);
-            switch (response){
-                case 0:
-                    System.out.println(qualifiers[randQuali] + " " + outputStr + "?");
-                    break;
-                case 1:
-                    System.out.println(hedges[randHed]);
-                    break;
-            }
-
-            if(userRespo.equalsIgnoreCase("q")){
-                System.out.println("Goodby! Thanks for using the program!");
-                userQuit = false;
-                break;
-            }
-        }while (userQuit == true);
-    }
+        System.out.println("Please ask your concern: (Q/q to quit)");
+        userInput  = keyboardInput.readLine();
+        
+     // While user input is not equal to the letter q keep looping
+        while(!userInput.equalsIgnoreCase("q")) {
+        	String output = ""; //output variable 
+        	splitStr = userInput.split(" "); // split the string by each words
+        	
+        	if (userInput.contains("I") || userInput.contains("me") || 
+        		userInput.contains("my") || userInput.contains("am")) {
+        	// Local instance of hash map is created
+        	HashMap<String, String> map = new HashMap<String, String>();
+        		map.put("I", "you");
+        		map.put("me", "you");
+        		map.put("my", "your");
+        		map.put("am", "are");
+        	for(int i=0; i<splitStr.length; i++) {
+        		if(splitStr[i].equalsIgnoreCase("my")) {
+        			splitStr[i] = map.get("my");
+        		}
+        		if(splitStr[i].equalsIgnoreCase("me")) {
+        			splitStr[i] = map.get("me");
+        		}
+        		if(splitStr[i].equalsIgnoreCase("I")) {
+        			splitStr[i] = map.get("I");
+        		}
+        		if(splitStr[i].equalsIgnoreCase("am")) {
+        			splitStr[i].equalsIgnoreCase("am");
+        		}
+        	}// End for
+        	
+        	// Concatenate the strings
+        		for(int i=0; i<splitStr.length; i++){
+	                output += splitStr[i] + " ";
+	            }
+        		
+        		// Random generator for choosing random response from the qualifiers array
+        		int randQual = rand.nextInt(qualifiers.length);
+	            System.out.println(qualifiers[randQual] + " " +  output + "?");
+	            userInput = keyboardInput.readLine();
+	            
+        	}else {
+        		// Random generator for choosing random response from the hedges array
+        		int randHedg = rand.nextInt(hedges.length);
+	            System.out.println(hedges[randHedg]);
+	            userInput = keyboardInput.readLine();
+        	}
+        }// End while loop 
+        System.out.println("Thank you for using the program! Bye!");
+	}
 }
